1ad345cc98cfdd0603f1d68fb34e4dbd94ffea8f
Maurice de Beijer <maurice.de.beijer@gmail.com>
2013-05-01 22:05:25 +0200

    Test

--- TheProblemSolver/Controllers/HomeController.cs
+++ TheProblemSolver/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace TheProblemSolver.Controllers
         {
             return View();
         }
+
+        public ActionResult Test()
+        {
+            return View();            
+        }
     }
 }
