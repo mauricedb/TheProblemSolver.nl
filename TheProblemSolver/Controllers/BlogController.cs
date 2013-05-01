67c1bd426c6b43a3457a0233982c0b275023963a
Maurice de Beijer <maurice.de.beijer@gmail.com>
2013-05-01 21:57:37 +0200

    Added blog items

---
+++ TheProblemSolver/Controllers/BlogController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Argotic.Common;
+using Argotic.Syndication;
+using TheProblemSolver.Models;
+
+namespace TheProblemSolver.Controllers
+{
+    public class BlogController : Controller
+    {
+        //public ActionResult TopStory()
+        //{
+        //    var blogItem = GetBlogFeed().First();
+        //    return PartialView(blogItem);
+            
+        //}
+
+
+        public ActionResult Recent()
+        {
+            var blogItems = GetBlogFeed().Take(5);
+            return PartialView(blogItems);
+        }
+
+        
+        private static List<BlogItem> _news;
+        private static DateTime _lastFetch;
+
+        private static IEnumerable<BlogItem> GetBlogFeed()
+        {
+            if (_lastFetch.AddMinutes(30) < DateTime.UtcNow)
+            {
+                _news = null;
+            }
+
+            if (_news == null)
+            {
+                try
+                {
+                    _news = GetTwitterFeedFromWeb();
+                    _lastFetch = DateTime.UtcNow;
+                }
+                catch (Exception)
+                {
+                }
+            }
+            return _news ?? Enumerable.Empty<BlogItem>();
+        }
+
+
+        private static List<BlogItem> GetTwitterFeedFromWeb()
+        {
+            var blogItems = new List<BlogItem>();
+            var feed = GetRssFeed();
+
+            foreach (var item in feed.Channel.Items)
+            {
+                var newsItem = new BlogItem
+                {
+                    Title = item.Title,
+                    Description = item.Description,
+                    Date = item.PublicationDate,
+                    Uri = item.Link
+                };
+
+                blogItems.Add(newsItem);
+            }
+
+            return blogItems;
+        }
+
+        private static RssFeed GetRssFeed()
+        {
+
+            var settings = new SyndicationResourceLoadSettings();
+            settings.RetrievalLimit = 9;
+
+            var feedUrl = new Uri("http://msmvps.com/blogs/theproblemsolver/rss.aspx");
+            var feed = RssFeed.Create(feedUrl, settings);
+
+            return feed;
+        }
+
+    }
+}
---
+++ TheProblemSolver/Models/BlogItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheProblemSolver.Models
+{
+    public class BlogItem
+    {
+        public DateTime Date { get; set; }
+        public Uri Uri { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
