e8b4715824d04ee2ed14fb146eaa076874260c73
Maurice de Beijer <mauricedb@computer.org>
2015-07-03 21:51:30 +0200

    React newsletter

---
+++ TheProblemSolver/Controllers/TheReactNewsletterController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace TheProblemSolver.Controllers
+{
+    public class TheReactNewsletterController : Controller
+    {
+        public ActionResult Subscribe()
+        {
+            return View();
+        }
+        
+    }
+}
