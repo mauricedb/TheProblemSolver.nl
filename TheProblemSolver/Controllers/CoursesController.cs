d945e2efad96ec226be0621f7a491ca95164515b
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-09-14 21:23:39 +0200

    Added Angular course

---
+++ TheProblemSolver/Controllers/CoursesController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace TheProblemSolver.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult React()
+        {
+            return View();
+        }
+
+        public ActionResult Angular()
+        {
+            return View();
+        }
+    }
+}
--- TheProblemSolver/Controllers/HomeController.cs
+++ TheProblemSolver/Controllers/HomeController.cs
@@ -35,17 +35,4 @@ namespace TheProblemSolver.Controllers
 #if !DEBUG
     [OutputCache(Duration = 15 * 60)]
 #endif
-    public class CoursesController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult React()
-        {
-            return View();
-        }
-
-    }
 }
