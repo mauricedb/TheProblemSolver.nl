2658d197f6a1a55e5db89d8e71feaab34d3d50cd
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-09-30 09:01:22 +0200

    Added basic bookie script

---
+++ TheProblemSolver/Scripts/bookieBookie.js
@@ -0,0 +1,21 @@
+﻿window.bookieBookie = () => {
+    function getPathFromUrl(url) {
+        return url.split(/[?#]/)[0];
+    }
+
+    const title = document.querySelector('title').textContent;
+    const url = encodeURIComponent(getPathFromUrl(document.location.href));
+
+    fetch(`https://api-ssl.bitly.com/v3/shorten?access_token=34b10af37f646d6d439967e20714315038621885&longUrl=${url}`)
+        .then(rsp => rsp.json())
+        .then(rsp => {
+            if (rsp.status_code === 200) {
+                console.log(title);
+                console.log(rsp.data.url);
+            } else {
+                console.log(rsp.status_code, rsp.status_txt);
+            }
+        });
+}
+
+bookieBookie();
