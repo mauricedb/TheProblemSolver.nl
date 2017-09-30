463a2eabb47965b3602a43c80ea87b3bf735f223
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-09-30 22:03:14 +0200

    Pass request to API

--- TheProblemSolver/Scripts/bookieBookie.js
+++ TheProblemSolver/Scripts/bookieBookie.js
@@ -7,30 +7,16 @@
     const url = encodeURIComponent(getPathFromUrl(document.location.href));
 	const image = document.querySelector('.progressiveMedia-image');
 	let imageSrc = null;
-	if (image){
-		imageSrc = image.src
+	if (image) {
+	    imageSrc = image.src;
 	}
-	
-    fetch(`https://api-ssl.bitly.com/v3/shorten?access_token=34b10af37f646d6d439967e20714315038621885&longUrl=${url}`)
-        .then(rsp => rsp.json())
-        .then(rsp => {
-            if (rsp.status_code === 200) {
-                console.log(title);
-                console.log(rsp.data.url);
-				console.log(imageSrc);
-            } else {
-                console.warn(rsp.status_code, rsp.status_txt);
 
-                console.log(title);
-				console.log(imageSrc);
-            }
-        })
-		.catch(e => {
-			console.warn(e);
+    console.log(title);
+    console.log(url);
+    console.log(imageSrc);
 
-			console.log(title);
-			console.log(imageSrc);
-		});
+    document.body.appendChild(document.createElement('script')).src =
+        `http://localhost:32662/Api/BookieBookie?url=${url}&title=${title}&image=${imageSrc}`;
 }
 
 bookieBookie();
