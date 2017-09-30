425a881318a4ab9a72b6945441665ebbee22670c
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-09-30 09:35:27 +0200

    Display error info

--- TheProblemSolver/Scripts/bookieBookie.js
+++ TheProblemSolver/Scripts/bookieBookie.js
@@ -5,17 +5,32 @@
 
     const title = document.querySelector('title').textContent;
     const url = encodeURIComponent(getPathFromUrl(document.location.href));
-
+	const image = document.querySelector('.progressiveMedia-image');
+	let imageSrc = null;
+	if (image){
+		imageSrc = image.src
+	}
+	
     fetch(`https://api-ssl.bitly.com/v3/shorten?access_token=34b10af37f646d6d439967e20714315038621885&longUrl=${url}`)
         .then(rsp => rsp.json())
         .then(rsp => {
             if (rsp.status_code === 200) {
                 console.log(title);
                 console.log(rsp.data.url);
+				console.log(imageSrc);
             } else {
-                console.log(rsp.status_code, rsp.status_txt);
+                console.warn(rsp.status_code, rsp.status_txt);
+
+                console.log(title);
+				console.log(imageSrc);
             }
-        });
+        })
+		.catch(e => {
+			console.warn(e);
+
+			console.log(title);
+			console.log(imageSrc);
+		});
 }
 
 bookieBookie();
