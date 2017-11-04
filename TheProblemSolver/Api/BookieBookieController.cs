a7ded8ac2607c430aeeb5851ab66304ab63c5c9a
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-11-04 21:24:45 +0100

    Simple storage

--- TheProblemSolver/Api/BookieBookieController.cs
+++ TheProblemSolver/Api/BookieBookieController.cs
@@ -1,68 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
-using Glimpse.Core.Tab;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Services;
-using Google.Apis.Sheets.v4;
-using Google.Apis.Sheets.v4.Data;
+using DotNetOpenAuth.Messaging;
 using Newtonsoft.Json.Linq;
 
 namespace TheProblemSolver.Api
 {
     public class BookieBookieController : ApiController
     {
-        public async Task<string> Get(string url, string title)
+        private static List<BookieBookieItem> items = new List<BookieBookieItem>();
+
+        public List<BookieBookieItem> Get()
         {
-            var shortUrl = await ShortenUrl(url);
-            await SaveInSpreadSheet(title, shortUrl);
-            return shortUrl;
+            var currentItems = items;
+
+            if (Request.RequestUri.Query.Contains("reset"))
+            {
+
+                items = new List<BookieBookieItem>();
+            }
+
+            return currentItems;
         }
 
-        private static async Task SaveInSpreadSheet(string title, string shortUrl)
+        public async Task<string> Get(string url, string title, string image)
         {
-            try
+            var shortUrl = await ShortenUrl(url);
+
+            items.Add(new BookieBookieItem()
             {
-                var clientId = Environment.GetEnvironmentVariable("BookieBookieClientId");
-                var clientSecret = Environment.GetEnvironmentVariable("BookieBookieSecret");
+                Title = title,
+                Url = shortUrl,
+                Image = image
+            });
 
-                var credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    new ClientSecrets()
-                    {
-                        ClientId = clientId,
-                        ClientSecret = clientSecret
-                    },
-                    new[] { SheetsService.Scope.Spreadsheets },
-                    "user",
-                    CancellationToken.None);
-
-                var sheetService = new SheetsService(new BaseClientService.Initializer()
-                {
-                    HttpClientInitializer = credential
-                });
-
-                var value = new ValueRange()
-                {
-                    Values = new List<IList<object>>()
-                };
-
-                value.Values.Add(new List<object>() { title, "User", shortUrl, DateTime.Today.ToString("yyyy-MM-dd") });
-
-                var sheetId = Environment.GetEnvironmentVariable("BookieBookieSheetId");
-                var request = sheetService.Spreadsheets.Values.Append(value, sheetId, "Sheet1!A2:D");
-                request.ValueInputOption =
-                    SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
-                var response2 = request.Execute();
-                Console.WriteLine(response2);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+            return shortUrl;
         }
 
         private static async Task<string> ShortenUrl(string url)
@@ -75,6 +49,14 @@ namespace TheProblemSolver.Api
             var json = JObject.Parse(response);
             var shortUrl = json["data"]["url"].ToString();
             return shortUrl;
+        }
+
+
+        public class BookieBookieItem
+        {
+            public string Title { get; set; }
+            public string Url { get; set; }
+            public string Image { get; set; }
         }
     }
 }
