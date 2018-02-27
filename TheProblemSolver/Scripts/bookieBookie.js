bdb6faa88f95319baae277f9f4dc316500ba442e
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-02-27 12:06:59 +0100

    Updated RegExp to make image link use 800

--- TheProblemSolver/Scripts/bookieBookie.js
+++ TheProblemSolver/Scripts/bookieBookie.js
@@ -1,26 +1,27 @@
 ﻿window.bookieBookie = () => {
-    function getPathFromUrl(url) {
-        return url.split(/[?#]/)[0];
-    }
+  function getPathFromUrl(url) {
+    return url.split(/[?#]/)[0];
+  }
 
-    const title = document.querySelector('title').textContent;
-    const url = encodeURIComponent(getPathFromUrl(document.location.href));
-    const image = document.querySelector('.progressiveMedia-image');
-    let imageSrc = null;
-    if (image) {
-        imageSrc = image.src;
-        const re = /https:\/\/cdn-images-1.medium.com\/max\/\d*\/(.*\.png)/i;
-        imageSrc.replace(re, 'https://cdn-images-1.medium.com/max/800/$1');
-    }
+  const title = document.querySelector('title').textContent;
+  const url = encodeURIComponent(getPathFromUrl(document.location.href));
+  const image = document.querySelector('.progressiveMedia-image');
+  let imageSrc = null;
+  if (image) {
+    imageSrc = image.src;
+    const re = /(https:\/\/cdn-images-1.medium.com\/max\/)\d*(\/.*\.png)/i;
+    imageSrc.replace(re, '$1' + '800' + '$2');
+  }
 
-    console.log(title);
-    console.log(url);
-    console.log(imageSrc);
+  console.log(title);
+  console.log(url);
+  console.log(imageSrc);
 
-    var host = 'https://theproblemsolver.azurewebsites.net';
-    //var host = 'http://localhost:32662';
-    document.body.appendChild(document.createElement('script')).src =
-        `${host}/Api/BookieBookie?url=${url}&title=${title}&image=${imageSrc}`;
-}
+  var host = 'https://theproblemsolver.azurewebsites.net';
+  //var host = 'http://localhost:32662';
+  document.body.appendChild(
+    document.createElement('script')
+  ).src = `${host}/Api/BookieBookie?url=${url}&title=${title}&image=${imageSrc}`;
+};
 
 bookieBookie();
